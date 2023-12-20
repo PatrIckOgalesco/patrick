039a25c9d5e7ac52fda83c578a73daea07199c81
PatrIck <81175383+PatrIckOgalesco@users.noreply.github.com>
2023-12-20 14:37:33 +0800

    Add files via upload

--- www/predictive-text.js
+++ www/predictive-text.js
@@ -1,77 +1,35 @@
-// // Array of predictive text options
-// let predictiveTextOptions = [];
-
-// fetch('www/getPredictiveText.php')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Predictive Text Options:', data);
-//         predictiveTextOptions = data;
-//         predictiveTextOptions.sort();
-//     })
-//     .catch(error => console.error('Error fetching predictive text options:', error));
-
-// const input = document.getElementById("chat-input");
-// const suggestion = document.getElementById("suggestion");
-
-// // Event listener for input changes
-// input.addEventListener("input", () => {
-//     clearSuggestion();
-//     const currentInput = input.value.trim(); // Trim leading and trailing spaces
-//     const regex = new RegExp("^" + currentInput, "i");
-
-//     const matchingSuggestions = predictiveTextOptions.filter(option =>
-//         regex.test(option.toLowerCase())
-//     );
-
-//     if (matchingSuggestions.length > 0 && currentInput) {
-//         matchingSuggestions.forEach(suggest => {
-//             const suggestionItem = document.createElement("div");
-//             suggestionItem.textContent = suggest;
-//             suggestionItem.classList.add("suggestion-item");
-//             suggestionItem.addEventListener("click", () => {
-//                 applySuggestion(suggest);
-//                 clearSuggestion();
-//             });
-//             suggestion.appendChild(suggestionItem);
-//         });
-//     }
-// });
-
-
-// // Event listener for Enter key press
-// input.addEventListener("keydown", (e) => {
-//     if (e.key === "Enter" && suggestion.childElementCount > 0) {
-//         e.preventDefault();
-//         applySuggestion(suggestion.textContent.trim());
-//         clearSuggestion();
-//     }
-// });
-
-// const applySuggestion = (selectedSuggestion) => {
-//     input.value = selectedSuggestion;
-//     clearSuggestion();
-// };
-
-
-
-// const clearSuggestion = () => {
-//     suggestion.innerHTML = "";
-// };
-
-
-
-
-
-
-
-
 
 // Array of predictive text options---------------suggestion forn js
 let predictiveTextOptions = [
-  "Where is Mr. Jomar's room?",
-  "Where is Mrs. Sheila's room?",
-  "history",
-  "who is gollum",
+"How can we reach you?",
+"What is your contact information?",
+"Can you share your email address?",
+"How can I get in touch with you?",
+"Tell me about your contact details.",
+
+"Tell me about yourself.",
+"Who are you?",
+"Share some information about you.",
+"Introduce yourself.",
+"What can you tell me about your background?",
+
+"Where did you study?",
+"Tell me about your education.",
+"What is your educational background?",
+"Can you share details about your schooling?",
+"Where are you currently studying?",
+
+"What skills do you have?",
+"Tell me about your expertise.",
+"What are your areas of specialization?",
+"Can you list your skills?",
+"What are you good at?",
+
+"What are you currently working on?",
+"Tell me about your current activities.",
+"What projects are you involved in?",
+"Share details about your current tasks.",
+"What is keeping you busy right now?",
 ];
 predictiveTextOptions.sort();
 
@@ -125,106 +83,3 @@ const clearSuggestion = () => {
 sendButton.style.display = "block";
 
 
-
-
-
-
-
-
-
-// // Array of predictive text options---------------2nd version not clickable ssuggestion
-// let predictiveTextOptions = [
-//   "Where is Mr. Jomar's room?",
-//   "Where is Mrs. Sheila's room?",
-//   "history",
-//   "who is gollum",
-// ];
-// predictiveTextOptions.sort();
-
-// const input = document.getElementById("chat-input");
-// const suggestion = document.getElementById("suggestion");
-
-// // Event listener for input changes
-// input.addEventListener("input", () => {
-//   clearSuggestion();
-//   const inputWords = input.value.split(" ");
-//   const lastWord = inputWords[inputWords.length - 1];
-//   const regex = new RegExp("^" + lastWord, "i");
-
-//   const matchingSuggestions = predictiveTextOptions.filter(option =>
-//     regex.test(option.toLowerCase())
-//   );
-
-//   if (matchingSuggestions.length > 0 && lastWord !== "") {
-//     matchingSuggestions.forEach(suggest => {
-//       const suggestionItem = document.createElement("div");
-//       suggestionItem.textContent = suggest;
-//       suggestion.appendChild(suggestionItem);
-//     });
-//   }
-// });
-
-// // Event listener for Enter key press
-// input.addEventListener("keydown", (e) => {
-//   if (e.key === "Enter" && suggestion.childElementCount > 0) {
-//     e.preventDefault();
-//     const currentInput = input.value;
-//     const updatedInput = currentInput.replace(/\S+$/, "");
-//     input.value = updatedInput + suggestion.textContent.trim();
-//     clearSuggestion();
-//   }
-// });
-
-// const clearSuggestion = () => {
-//   suggestion.innerHTML = "";
-// };
-
-// sendButton.style.display = "block";
-
-
-
-
-
-
-
-
-
-// 1st version
-// let predictiveTextOptions = [
-//   "Where is Mr. Jomar's room?",
-//   "Where is Mrs. Sheila's room?",
-//   "history",
-//   "who is gollum",
-// ];
-// predictiveTextOptions.sort();
-
-// const input = document.getElementById("chat-input");
-// const suggestion = document.getElementById("suggestion");
-
-// // Event listener for input changes
-// input.addEventListener("input", () => {
-//   clearSuggestion();
-//   const regex = new RegExp("^" + input.value, "i");
-
-//   for (let i in predictiveTextOptions) {
-//     if (regex.test(predictiveTextOptions[i]) && input.value !== "") {
-//       suggestion.textContent = predictiveTextOptions[i];
-//       break;
-//     }
-//   }
-// });
-
-// // Event listener for Enter key press
-// input.addEventListener("keydown", (e) => {
-//   if (e.key === "Enter" && suggestion.textContent !== "") {
-//     e.preventDefault();
-//     input.value = suggestion.textContent;
-//     clearSuggestion();
-//   }
-// });
-
-// const clearSuggestion = () => {
-//   suggestion.textContent = "";
-// };
-
-// sendButton.style.display = "block";
--- www/script.js
+++ www/script.js
@@ -14,13 +14,13 @@ const loadDataFromLocalstorage = () => {
   const defaultText = `<div class="default-text"><br><br>
     <h1>Hello, There!</h1><br>
     <div class="default-sub" id="default-sub">
-        <p style="font-size: 12px;">"It's PatrIck Ogalesco. Explore my skills, projects, and more. Feel free to ask me anything!"</p>
-      </div>
+        <p style="font-size: 12px;">"It's PatrIck Ogalesco – a passionate individual on a journey of exploration and creation. Explore my skills, projects, and more. Feel free to ask me anything!"</p>
+    </div>
 
 
-      <div id="default-sub2">
-        <p style="font-size: 12px;">"I am a passionate creator, seamlessly blending the worlds of programming and digital art. With a keyboard as my canvas and code as my brush, I craft immersive digital experiences that transcend the boundaries of technology and aesthetics."</p>
-      </div>
+    <div id="default-sub2">
+        <p style="font-size: 12px;">I am <span id="typewriter"></span><span id="cursor">|</span></p>
+    </div>
     </div>
   </div>`;
   
@@ -45,14 +45,14 @@ const displayAnswerWithTypingEffect = async (answer) => {
 
   const incomingChatDiv = document.createElement("div");
   incomingChatDiv.classList.add("chat", "incoming");
-  incomingChatDiv.innerHTML = answerHtml;
+  incomingChatDiv.innerHTML = answerHtml; // Use innerHTML here
 
   chatContainer.appendChild(incomingChatDiv);
   chatContainer.scrollTo(0, chatContainer.scrollHeight);
 
   const responseText = document.querySelector(".incoming:last-child p");
   for (let i = 0; i < answer.length; i++) {
-    responseText.textContent = answer.substring(0, i + 1);
+    responseText.innerHTML = answer.substring(0, i + 1); // Use innerHTML here
     await sleep(50);
   }
 };
@@ -78,21 +78,137 @@ const handleOutgoingChat = async () => {
   const question = userText.toLowerCase();
 
   switch (question) {
-    case 'hi':
-    case 'hello':
-      const greetingResponse = "Hi there! How can I assist you?";
-      // Use typing-text animation function instead of direct HTML response
-      displayAnswerWithTypingEffect(greetingResponse);
-      break;
+  case 'hi':
+  case 'hello':
+    const greetingResponse = "Hi there! How can I assist you?";
+    // Use typing-text animation function instead of direct HTML response
+    displayAnswerWithTypingEffect(greetingResponse);
+    break; // Add the missing semicolon here
+  case 'about you':
+  case 'tell me about yourself':
+  case 'who are you?':
+  case 'share some information about you':
+  case 'introduce yourself':
 
-    // Add more cases for different user inputs
+    const aboutMeResponse = `
+      Hello there, I am Patrick Ogalesco, an aspiring digital artist, boasting a unique fusion of technical prowess and artistic finesse. 
+      Currently, I am passionately pursuing a BSc in Computer Science, immersing myself not only in the realm of coding and programming but also dedicating myself fervently to the canvas as an artist. <br><br>
+      My journey seamlessly weaves together creativity and logic, charting a course toward innovative problem-solving and the creation of visually breathtaking solutions. Fueled by an innate curiosity and an unwavering commitment to bridging the gap between art and technology, I am poised to inject a fresh perspective into the world of digital art.
+    `;
+    displayAnswerWithTypingEffect(aboutMeResponse);
+    break;
 
-    default:
-      const defaultResponse = "I'm sorry, I didn't understand that. Can you please provide more information?";
-      // Use typing-text animation function instead of direct HTML response
-      displayAnswerWithTypingEffect(defaultResponse);
-      break;
-  }
+    case 'education':
+    case 'where did you study?':
+    case 'tell me about your education':
+    case 'what is your educational background?':
+    case 'can you share details about your schooling?':
+    case 'where are you currently studying?':
+    const educationResponse = `
+    Education
+    Here is a summary of my educational journey:
+    - Christian Polytechnic Institute Of Catanduanes (2021 - 2024)
+    - Catanduanes College (2020 - 2021)
+    - San Jose(OCO) National High School (2014 - 2020)
+    - Sagrada Viga Catanduanes (2008 - 2013)
+  `;
+  displayAnswerWithTypingEffect(educationResponse);
+  break;
+  case 'area of expertise':
+  const expertiseResponse = `
+     I specialize in various skills that contribute to my expertise in the field. Here are some of my key areas:
+    - Photoshop
+    - Ibis Paint
+    - Krita
+    - HTML And CSS
+    - JavaScript
+    - PHP
+    - C
+    - C++
+    - JAVA
+    - Visual Basic
+    - Microsoft Application
+  `;
+  displayAnswerWithTypingEffect(expertiseResponse);
+  break;
+
+
+  case "what you do":
+  case 'what are you up to?':
+  case 'tell me about your current projects':
+  case 'what are you currently learning?':
+  case 'share details about your ongoing tasks':
+  case 'what keeps you occupied?':
+
+  const doingResponse = `Here's an overview of what I'm currently engaged in:
+
+Web Design
+- Currently in the process of learning.
+
+Web Development
+- Currently in the process of learning.
+
+Android Development
+- Currently in the process of learning.
+
+Digital Art
+- Currently in the process of learning.`;
+  displayAnswerWithTypingEffect(doingResponse);
+  break;
+
+
+
+  case 'skills':
+  case 'what are your skills?':
+  case 'tell me about your abilities':
+  case 'what can you do?':
+  case 'can you list your skills?':
+  case 'share information about your talents':
+
+  const skillsResponse = `
+    As a versatile individual with a passion for both technology and creativity, here are some of my key skills:
+
+    - Digital Art
+    - Programming
+    - Web Development
+    - Artistic Vision
+    - Collaboration
+    - Adaptability
+    - Attention to Details
+    - Continuous Learning
+  `;
+  displayAnswerWithTypingEffect(skillsResponse);
+  break;
+
+  case 'contact':
+  case 'how can we reach you?':
+  case 'what is your contact information?':
+  case 'can you share your email address?':
+  case 'how can I get in touch with you?':
+  case 'tell me about your contact details':
+    const contactResponse = `
+      EMAIL
+      jpatrickogalesco@gmail.com
+
+      PHONE
+      0938 070 ****
+
+      LOCATION
+      Viga Catanduanes
+    `;
+    displayAnswerWithTypingEffect(contactResponse);
+    break;
+
+
+
+
+
+  default:
+    const defaultResponse = "I'm sorry, I didn't understand that. My responses are generated based on programmed commands.";
+    displayAnswerWithTypingEffect(defaultResponse);
+    break;
+}
+
 };
 
 const deleteChats = () => {
